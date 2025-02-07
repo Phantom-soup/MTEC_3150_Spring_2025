84cbc4f6e0b53bb28685db7f3b29741d984fe5f3
rakinola-citytech <rukayat.akinola@mail.citytech.cuny.edu>
2025-02-07 15:56:50 -0500

    02_05 (FirstClassEdits
    JuiceUP AsteriodLike 2D Game

--- Lecture1/JuiceItUp/Assets/Meteor.cs
+++ Lecture1/JuiceItUp/Assets/Meteor.cs
@@ -9,11 +9,16 @@ public class Meteor : iDamageable
     [SerializeField]
     private float DeathTime = 6f;
     public float InitialForce=100f;
+    SpriteRenderer sr;
+
     void Start()
     {
         StartTime = Time.realtimeSinceStartup;
         Vector2 PositionVector = GameObject.FindObjectOfType<Ship>().transform.position - transform.position;
         GetComponent<Rigidbody2D>().AddForce(PositionVector.normalized * InitialForce);
+
+        //some edits
+        sr = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,10 +32,11 @@ public class Meteor : iDamageable
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.GetComponent<Ship>() != null)
+        if (collision.gameObject.GetComponent<Ship>() != null)
         {
             collision.gameObject.GetComponent<Ship>().DoDamage();
         }
+        
     }
 
 
@@ -41,11 +47,33 @@ public class Meteor : iDamageable
 
     public void onDestroyDamage()
     {
-        Destroy(this.gameObject);
+        StartCoroutine(BlinkRed());
+        //Destroy(this.gameObject);
     }
     public void onDestroyTime()
     {
         Destroy(this.gameObject);
+    }
+
+    public IEnumerator BlinkRed()
+    {
+        WaitForSeconds delay = new WaitForSeconds(0.05f);
+
+        //canTakeDamage = false;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        Destroy(this.gameObject);
+        //canTakeDamage = true;
     }
 
 }
