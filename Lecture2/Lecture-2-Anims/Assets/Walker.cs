4a563c110ac4213acddb725f0efa53abcfa7bcc7
hosoji <CHARLES.GEORGE55@citytech.cuny.edu>
2025-02-19 21:01:51 -0500

    Lecture 2

--- Lecture2/Lecture-2-Anims/Assets/Walker.cs
+++ Lecture2/Lecture-2-Anims/Assets/Walker.cs
@@ -6,22 +6,39 @@ public class Walker : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 3.0f;
-    public Vector2 Veloctiy;
+    public Vector2 Velocity;
+    bool IsWalking;
     public Rigidbody2D rigidBody2D;
+    public Animator animator;
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Veloctiy = Vector2.zero;
-       Veloctiy = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Veloctiy.Normalize();
-        Veloctiy *= speed;
-        rigidBody2D.velocity   = Veloctiy;
-        
+        Velocity = Vector2.zero;
+        Velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        float NormalizedSpeed = Velocity.magnitude;
+
+        if(NormalizedSpeed > .2f)
+        {
+            IsWalking = true;
+        }
+        else
+        {
+            IsWalking = false;
+        }
+        Velocity.Normalize();
+        animator.SetFloat("Horizontal", Velocity.x);
+        animator.SetFloat("Vertical", Velocity.y);
+        animator.SetBool("IsWalking", IsWalking);
+
+        Velocity *= speed;
+        rigidBody2D.velocity   = Velocity;
     }
 }
     
